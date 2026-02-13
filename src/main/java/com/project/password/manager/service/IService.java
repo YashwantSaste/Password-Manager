b7523c117bd9238e1df91eda9416ec8d1e8d2aa5
Yashwant Saste <yashwant.saste.ext@siemens.com>
2026-02-13 23:55:34 +0530

    Initializing the User Service

---
+++ src/main/java/com/project/password/manager/service/IService.java
@@ -0,0 +1,5 @@
+package com.project.password.manager.service;
+
+public interface IService {
+	// no concrete methods for now will be adding in the further code
+}
---
+++ src/main/java/com/project/password/manager/service/UserService.java
@@ -0,0 +1,20 @@
+package com.project.password.manager.service;
+
+import org.jetbrains.annotations.NotNull;
+
+import com.project.password.manager.database.DataRepository;
+import com.project.password.manager.model.IUser;
+
+public class UserService implements IService {
+
+	@NotNull
+	private final DataRepository<IUser, String> userRepository;
+
+	public UserService(@NotNull DataRepository<IUser, String> userRepository) {
+		this.userRepository = userRepository;
+	}
+
+	public IUser getUser(@NotNull String id) {
+		return userRepository.findById(id);
+	}
+}
